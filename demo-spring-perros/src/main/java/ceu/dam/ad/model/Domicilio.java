3fdb6b7dc239ad2e18a37a5d8ea934750a5f909e
ft <tr>
2025-11-04 20:57:59 +0100

    t

--- demo-spring-perros/src/main/java/ceu/dam/ad/model/Domicilio.java
+++ demo-spring-perros/src/main/java/ceu/dam/ad/model/Domicilio.java
@@ -1,12 +1,10 @@
 package ceu.dam.ad.model;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,13 +13,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "domicilios")
 public class Domicilio {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String calle;
 	private Integer numero;
-
-
 
 }
